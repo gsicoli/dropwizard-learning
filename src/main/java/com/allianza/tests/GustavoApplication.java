1f889f49bcb4b0f55228f5c001ed3628978847e9
Gustavo Sicoli <gsicoli@daitan.com>
2019-07-05 11:41:54 -0300

    trying to make it run...

--- src/main/java/com/allianza/tests/GustavoApplication.java
+++ src/main/java/com/allianza/tests/GustavoApplication.java
@@ -5,25 +5,29 @@ import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class GustavoApplication extends Application<GustavoConfiguration> {
+  public static void main(final String[] args) throws Exception {
+    new GustavoApplication().run(args);
+  }
 
-    public static void main(final String[] args) throws Exception {
-        new GustavoApplication().run(args);
-    }
+  @Override
+  public String getName() {
+    return "Gustavo";
+  }
 
-    @Override
-    public String getName() {
-        return "Gustavo";
-    }
+  @Override
+  public void initialize(final Bootstrap<GustavoConfiguration> bootstrap) {
+    // TODO: application initialization
+  }
 
-    @Override
-    public void initialize(final Bootstrap<GustavoConfiguration> bootstrap) {
-        // TODO: application initialization
-    }
-
-    @Override
-    public void run(final GustavoConfiguration configuration,
-                    final Environment environment) {
-        // TODO: implement application
-    }
-
+  @Override
+  public void run(final GustavoConfiguration configuration,
+                  final Environment environment) {
+    final HelloWorldResource resource = new HelloWorldResource(
+      configuration.getTemplate(),
+      configuration.getDefaultName()
+    );
+    final TemplateHealthCheck healthCheck = new TemplateHealthCheck(configuration.getTemplate());
+    environment.healthChecks().register("template", healthCheck);
+    environment.jersey().register(resource);
+  }
 }
